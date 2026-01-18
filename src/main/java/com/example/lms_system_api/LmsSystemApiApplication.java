311b6d013440cd601217623b6f835d2a5616ae2d
saimo <saimonderc@gmail.com>
2026-01-18 21:44:29 +0500

    initial commit

---
+++ src/main/java/com/example/lms_system_api/LmsSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.lms_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lms_system_api/LmsSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lms_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
